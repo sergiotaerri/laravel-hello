690d3b859056541e3ddcad74decd2497151e930e
sergiotaerri <sergio2000jr@gmail.com>
2020-10-15 12:08:42 -0300

    Swagger integration init

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,31 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+     /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="API de cadastro de endereços",
+     *      description="L5 Swagger OpenApi description",
+     *      @OA\Contact(
+     *          email="admin@admin.com"
+     *      ),
+     *      @OA\License(
+     *          name="Apache 2",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Demo API Server"
+     * )
+
+     *
+     * @OA\Tag(
+     *     name="Enderecos",
+     *     description="API Endpoints of Projects"
+     * )
+     */
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
--- app/Http/Controllers/EnderecoController.php
+++ app/Http/Controllers/EnderecoController.php
@@ -6,13 +6,34 @@ use App\Models\Endereco;
 use Illuminate\Http\Request;
 use App\Http\Requests\EnderecoValidated;
 
+/**
+ * @OA\Info(
+ *    title="API de Endereço",
+ *    version="1.0.0",
+ * )
+ */
 class  EnderecoController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\JsonResponse
-     */
+     * 
+     * @OA\Get(
+     *  path="/enderecos",
+     *  description="Retorna os endereços armazenados no banco de dados",
+     *  @OA\Parameter(
+     *      name="id",
+     *      in="query",
+     *      type"string",
+     *      description="Id do endereço",
+     *  ),
+     *  @OA\Response(
+     *      response=201,
+     *      description="Recourse created"
+     *  )
+     * )
+    */
     public function index()
     {
 
@@ -20,11 +41,19 @@ class  EnderecoController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
-     */
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\JsonResponse
+    * @OA\Post(
+    *   path="/enderecos",
+    *   description="Adiciona um endereço",
+    *   @OA\Response(
+    *       response=201,
+    *       description="Recourse created"
+    *   )
+    * )
+    */
     public function store(Endereco $request)
     {
         $endereco_validado = $request->validated()
@@ -34,23 +63,53 @@ class  EnderecoController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    * @OA\Get(
+    *   path="/enderecos/{id}",
+    *   description="Retorna os endereços armazenados no banco de dados",
+    *   @OA\Parameter(
+    *       name="id",
+    *       in="query",
+    *       type"string",
+    *       description="Id do endereço",
+    *   ),
+    *   @OA\Response(
+    *       response=200,
+    *       description="Recurso entregue"
+    *   )
+    * )
+    */
     public function show($id)
     {
-        //
+        $endereco = Endereco::findOrFail($id);
+
+        return response()->json($endereco, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\JsonResponse
-     */
+   /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\JsonResponse
+    * @OA\Put(
+    *   path="/enderecos/{id}",
+    *   description="Retorna os endereços armazenados no banco de dados",
+    *   @OA\Parameter(
+    *       name="id",
+    *       in="query",
+    *       type"string",
+    *       description="Id do endereço",
+    *   ),
+    *   @OA\Response(
+    *       response=200,
+    *       description="Recurso atualizado no banco"
+    *   )
+    *  )
+    */
     public function update(Endereco $request, $id)
     {
         $endereco = Endereco::findOrFail($id);
@@ -66,6 +125,23 @@ class  EnderecoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\JsonResponse
      */
+
+     /**
+    *   @OA\Put(
+    *   path="/enderecos/{id}",
+    *   description="Retorna os endereços armazenados no banco de dados",
+    *   @OA\Parameter(
+    *       name="id",
+    *       in="query",
+    *       type"string",
+    *       description="Id do endereço",
+    *   ),
+    *   @OA\Response(
+    *       response=204,
+    *           description="Recurso deletado do banco"
+    *   )
+    * )
+ */
     public function destroy($id)
     {
         Endereco::findOrFail($id)->delete();
